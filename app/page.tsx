7488b315e2d30e4a7c8f99b1e433a00fa519ca12
Kapollo <kapobear9509@gmail.com>
2023-11-28 11:32:29 +0800

    init page & create function to get movie list

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// 1. Init API key
 const API_KEY = '90c95f605d644100846fc86f59c97f0e';
 
-// 1. Init data type
 type Movie = {
   id: number;
   title: string;
@@ -29,11 +27,147 @@ type MovieDetails = {
   budget: number;
   backdrop_path: string;
 };
-//
+
+
 
 const Home = () => {
+  
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<number>(0);
+  const [sortBy, setSortBy] = useState<string>('title');
+  const [rating, setRating] = useState<number>(0);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [movieDetails, setMovieDetails] = useState<MovieDetails | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(30);
+
+  const fetchMovies = async () => {
+    try {
+      if (currentPage % 2 === 1) {
+        const pageNum = (currentPage + 1) / 2 * 3 - 2;
+        const response1 = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${pageNum}`);
+        const response2 = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${pageNum + 1}`);
+        let result = response1.data.results.concat(response2.data.results).slice(0, 30);
+        setTotalPages(Math.round(response1.data.total_pages * 20 / 30));
+        setMovies(result);
+      } else {
+        const pageNum = currentPage / 2 * 3 - 1;
+        const response1 = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${pageNum}`);
+        const response2 = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${pageNum + 1}`);
+        let result = response1.data.results.concat(response2.data.results).slice(10, 40);
+        setTotalPages(Math.round(response1.data.total_pages * 20 / 30));
+        setMovies(result);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchGenres = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
+      );
+      setGenres(response.data.genres);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGenres(); // Get genre list
+  }, []);
+
+  useEffect(() => {
+    fetchMovies(); // Get movie list
+  }, [currentPage]);
+
   return (
-    <div>Welcome</div>    
+    <div className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold mt-8 mb-4">Movies</h1>
+      <div className="flex justify-between items-center mb-4">
+        <div>
+          <label className="mr-2">Genre:</label>
+          <select
+            value={selectedGenre}
+            onChange={handleGenreChange}
+            className="border rounded px-2 py-1 text-black"
+          >
+            <option value={0}>All</option>
+            {genres.map((genre) => (
+              <option key={genre.id} value={genre.id}>
+                {genre.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label className="mr-2">Rating:</label>
+          <input
+            type="number"
+            min={0}
+            max={10}
+            step={0.1}
+            value={rating}
+            onChange={handleRatingChange}
+            className="border rounded px-2 py-1 w-20 text-black"
+          />
+        </div>
+        <div>
+          <label className="mr-2">Sort By:</label>
+          <select
+            value={sortBy}
+            onChange={handleSortChange}
+            className="border rounded px-2 py-1 text-black"
+          >
+            <option value='title'>Title</option>
+            <option value='release_date'>Release Date</option>
+            <option value='popularity'>Popularity</option>
+            <option value='vote_average'>Rating</option>
+          </select>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        {sortMovies(movies)
+          .filter(
+            (movie) =>
+              (!selectedGenre || movie.genre_ids.includes(selectedGenre)) &&
+              (!rating || movie.vote_average >= rating)
+          )
+          .map((movie) => (
+            <div
+              key={movie.id}
+              className="flex flex-col items-center cursor-pointer"
+              onClick={() => handleMovieClick(movie)}
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                className="mb-2 w-40 h-60 object-cover rounded"
+              />
+              <p className="text-center">{movie.title}</p>
+              <p className="text-gray-500">{movie.release_date}</p>
+            </div>
+          ))}
+      </div>
+      <div className="flex justify-center mt-8">
+        <button
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l"
+        >
+          Previous Page
+        </button>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r"
+        >
+          Next Page
+        </button>
+      </div>
+    </div>
   );
 };
 
